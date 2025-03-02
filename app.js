533c0595c3894735bfc1afcde742a8a267aae977
Navaneeth Gade <navaneethgade07@gmail.com>
2025-03-02 11:38:39 +0530

    Updated Package Versions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRouter = require('./routes/user');
 const article = require('./models/article');
 const connectDB = require('./db/connect');
 const methodOverride = require('method-override')
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const { createToken, validateToken } = require('./utils/token');
 require('dotenv').config();
 
